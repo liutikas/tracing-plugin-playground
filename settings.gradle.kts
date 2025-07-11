422f3e706ad661ec807e8723532a9d1f40fc31aa
Aurimas Liutikas <aurimas@google.com>
2025-07-11 09:18:11 -0700

    Upgrade

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
         mavenCentral()
         google()
         maven {
-            url = uri("https://androidx.dev/snapshots/builds/13449038/artifacts/repository")
+            url = uri("https://androidx.dev/snapshots/builds/13768719/artifacts/repository")
         }
         gradlePluginPortal()
     }
@@ -21,5 +21,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "smallest-settings-plugin"
+rootProject.name = "tracing-plugin-playground"
 include("libA")
